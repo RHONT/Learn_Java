b7be2e1fb5bc6d5daa2b9c7fc042f70344e4ae80
RHONT <rg915i@yandex.ru>
2021-12-14 12:36:08 +0300

    Сортируем массив ест. сп. и удаляем дубликаты

--- Java_my_examples/Begin/src/array/ArrayTo.java
+++ Java_my_examples/Begin/src/array/ArrayTo.java
@@ -9,32 +9,24 @@ public class ArrayTo {
     static ArrayList<String> abc(String[] a) {
         ArrayList<String> bufer = new ArrayList<>();
         ArrayList<String> getter = new ArrayList<>();
-        String[] b;
-        System.out.println(Arrays.toString(a));
-        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));  // вывод изначального массива
+        Arrays.sort(a);                          // Сортируем массив естественным способом
         Collections.addAll(bufer, a);
-        System.out.println(bufer.toString());
+        System.out.println(bufer);
         for (int i = 0; (i < (bufer.size() - 1)); i++) {
             if (((bufer.get(i)).equals(bufer.get(i + 1))) == false) {
                 getter.add(bufer.get(i));
             }
         }
         bufer = (ArrayList) getter.clone();
-        b = (String[]) bufer.clone();
         getter.clear();
         return bufer;
 
     }
 
     public static void main(String[] args) {
-        ArrayList<String> bufer = new ArrayList<>();
-        bufer.add("1");
-        bufer.add("2");
-        String[] b;
-        //b=bufer.clone();
 
-        b = (String[]) bufer.clone();
         System.out.println(abc(new String[]{"c", "dsaf", "adsfadf", "fasdf", "a", "a", "b", "b", "c"}));
-        System.out.println(Arrays.toString(b));
+
     }
 }
