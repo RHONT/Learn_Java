058ac0ed751402bb64150b07be3b8be736e73ec5
RHONT <rg915i@yandex.ru>
2021-12-14 21:25:45 +0300

    Задача - терраформинг планеты, после чего выпускаются звери.

---
+++ Java_my_examples/Begin/src/Multi_Threading/Count_Down_Latch.java
@@ -0,0 +1,81 @@
+package Multi_Threading;
+
+import java.util.concurrent.CountDownLatch;
+
+//Просиходит терраформация земли. И только спустя пять операций начинают добавляться животные.
+
+public class Count_Down_Latch {
+    public static void main(String[] args) throws InterruptedException {
+        Animal a1=new Animal("Гусеница",cdl);
+        new Animal("Белка",cdl);
+        new Animal("Лось",cdl);
+        new Animal("Мамонт",cdl);
+
+        create_planet();
+        create_sphere();
+        create_water();
+        create_mountains();
+        create_forest();
+
+
+
+        System.out.println("поток main прекратил существование"); // Поток main завершил свою работу сразу как CDL стал равен 0
+
+    }
+
+    static CountDownLatch cdl=new CountDownLatch(5);
+
+    private static void create_planet() throws InterruptedException {
+
+        Thread.sleep(1000);
+        System.out.println("В мертвом космосе зародилась планета");
+        cdl.countDown();
+    }
+    private static void create_water() throws InterruptedException {
+
+        Thread.sleep(1000);
+        System.out.println("Из небытия ледяные пласты рухнул поток воды.");
+        cdl.countDown();
+    }
+    private static void create_sphere() throws InterruptedException {
+
+        Thread.sleep(1000);
+        System.out.println("Планету окружила атмосфера");
+        cdl.countDown();
+    }
+    private static void create_mountains() throws InterruptedException {
+
+        Thread.sleep(1000);
+        System.out.println("Из водный глади выросли горы");
+        cdl.countDown();
+    }
+    private static void create_forest() throws InterruptedException {
+
+        Thread.sleep(1000);
+        System.out.println("На голой суше появились первые зеленые ростки");
+        cdl.countDown();
+    }
+
+
+}
+
+class Animal extends Thread {
+    String name;
+    CountDownLatch cdl;
+
+    public Animal(String name, CountDownLatch cdl) {
+        this.name = name;
+        this.cdl = cdl;
+        this.start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            cdl.await();
+            System.out.println(name + " Выбежал(а) на сушу!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
